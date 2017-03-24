197506ed51bd55c64f5b7a9dea32c8ff410a6c02
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-24 17:48:50 +0100

    load swagger configuration

--- user-service/src/main/java/io/crowdcode/flaschenlager/user/UserServiceApplication.java
+++ user-service/src/main/java/io/crowdcode/flaschenlager/user/UserServiceApplication.java
@@ -1,9 +1,12 @@
 package io.crowdcode.flaschenlager.user;
 
+import io.crowdcode.commons.swagger.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
