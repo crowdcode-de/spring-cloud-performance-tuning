3984eec4dfffc580c20b85547ff2b7869f493e8d
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-24 19:34:22 +0100

    add simple test to check application context

---
+++ user-service/src/test/java/io/crowdcode/flaschenlager/user/UserServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package io.crowdcode.flaschenlager.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceApplicationTest {
+
+    @Test
+    public void testIfSpringBootContextCanBeStartet() throws Exception {
+        // this test is intentional empty
+    }
+}
