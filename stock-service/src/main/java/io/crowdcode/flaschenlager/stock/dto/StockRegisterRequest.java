89ee6db78afcb2aad45a4468e2fcf59091fc72d3
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-26 00:35:18 +0100

    add StockReqisterRequest

---
+++ stock-service/src/main/java/io/crowdcode/flaschenlager/stock/dto/StockRegisterRequest.java
@@ -0,0 +1,25 @@
+package io.crowdcode.flaschenlager.stock.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@EqualsAndHashCode()
+@ToString()
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockRegisterRequest {
+
+    @NotNull
+    private String name;
+
+}
