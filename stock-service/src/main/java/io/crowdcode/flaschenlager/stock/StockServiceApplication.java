579cc81a8023c43b8022b69f1e340125775156b2
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-25 18:38:29 +0100

    add spring boot application to stock-service

---
+++ stock-service/src/main/java/io/crowdcode/flaschenlager/stock/StockServiceApplication.java
@@ -0,0 +1,12 @@
+package io.crowdcode.flaschenlager.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockServiceApplication.class, args);
+    }
+}
