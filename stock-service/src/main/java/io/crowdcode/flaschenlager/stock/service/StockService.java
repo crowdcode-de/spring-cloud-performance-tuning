294aaea7de0c01a9db1cef0b95fa9432d4f1af47
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-25 20:28:23 +0100

    add quantity request

--- stock-service/src/main/java/io/crowdcode/flaschenlager/stock/service/StockService.java
+++ stock-service/src/main/java/io/crowdcode/flaschenlager/stock/service/StockService.java
@@ -1,12 +1,17 @@
 package io.crowdcode.flaschenlager.stock.service;
 
 import io.crowdcode.flaschenlager.stock.model.Stock;
+import io.crowdcode.flaschenlager.stock.model.StockEntry;
+import io.crowdcode.flaschenlager.stock.model.StockEntryQuantity;
 import io.crowdcode.flaschenlager.stock.repository.StockEntryRepository;
 import io.crowdcode.flaschenlager.stock.repository.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional
@@ -18,7 +23,25 @@ public class StockService {
     @Autowired
     private StockEntryRepository stockEntryRepository;
 
-    public Stock registerStock(String stockName) {
-        return stockRepository.save(new Stock().setName(stockName));
+    public Long registerStock(String stockName) {
+        return stockRepository.save(new Stock().setName(stockName)).getId();
     }
+
+    public List<StockEntryQuantity> calculateQuantities() {
+        return stockEntryRepository.findStockQuantities();
+    }
+
+    public void put(Long stockId, Long productId, Long quantity, double price) {
+        Stock stock = stockRepository.findOne(stockId);
+
+        StockEntry entry = new StockEntry()
+                .setStock(stock)
+                .setAddedAt(LocalDateTime.now())
+                .setProductId(productId)
+                .setQuantity(quantity)
+                .setPrice(BigDecimal.valueOf(price));
+
+        stockEntryRepository.save(entry);
+    }
+
 }
--- stock-service/src/test/java/io/crowdcode/flaschenlager/stock/service/StockServiceTest.java
+++ stock-service/src/test/java/io/crowdcode/flaschenlager/stock/service/StockServiceTest.java
@@ -1,27 +1,47 @@
 package io.crowdcode.flaschenlager.stock.service;
 
 
-import io.crowdcode.flaschenlager.stock.model.Stock;
+import io.crowdcode.flaschenlager.stock.model.StockEntryQuantity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class StockServiceTest {
 
+    public static final String STOCK = "STOCK_NAME_BY_SERVICE";
     @Autowired
     private StockService stockService;
 
     @Test
     public void testRegisterStock() throws Exception {
-        Stock stock = stockService.registerStock("STOCK_NAME_BY_SERVICE");
-        assertThat(stock.getId(), is(notNullValue()));
+        Long stockId = stockService.registerStock("STOCK_NAME_BY_SERVICE");
+        assertThat(stockId, is(notNullValue()));
+    }
+
+    @Test
+    public void testStockQuantities() throws Exception {
+        Long stockId = stockService.registerStock(STOCK);
+        stockService.put(stockId, 1l, 1l, 1.1);
+        stockService.put(stockId, 1l, 2l, 1.2);
+        stockService.put(stockId, 2l, 3l, 1.3);
+        stockService.put(stockId, 2l, 4l, 1.4);
+        stockService.put(stockId, 3l, 0l, 1.5);
+        stockService.put(stockId, 3l, 0l, 1.6);
+
+        List<StockEntryQuantity> quantities = stockService.calculateQuantities();
+
+        assertThat(quantities, hasSize(2));
+        assertThat(quantities, containsInAnyOrder(new StockEntryQuantity(1l, 3l), new StockEntryQuantity(2l, 7l)));
     }
 }
