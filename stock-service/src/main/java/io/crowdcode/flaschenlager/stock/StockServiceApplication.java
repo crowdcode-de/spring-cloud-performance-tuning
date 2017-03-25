63ca6d13715e957ae74cdaa8ea105400b5aa71d2
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-25 21:28:56 +0100

    enable LocalDateTime converter

--- stock-service/src/main/java/io/crowdcode/flaschenlager/stock/StockServiceApplication.java
+++ stock-service/src/main/java/io/crowdcode/flaschenlager/stock/StockServiceApplication.java
@@ -1,8 +1,12 @@
 package io.crowdcode.flaschenlager.stock;
 
+import io.crowdcode.commons.jpa.annotations.EnableCommonsJpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan // due to enable commons jpa
+@EnableCommonsJpa
 @SpringBootApplication
 public class StockServiceApplication {
 
