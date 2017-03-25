315225ae7ab75e6830987f1a59086ebd93e38a49
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-26 00:35:53 +0100

    add store controller and controller tests

---
+++ stock-service/src/main/java/io/crowdcode/flaschenlager/stock/controller/StockController.java
@@ -0,0 +1,70 @@
+package io.crowdcode.flaschenlager.stock.controller;
+
+import io.crowdcode.flaschenlager.stock.dto.EntryPullRequest;
+import io.crowdcode.flaschenlager.stock.dto.EntryReceiptRequest;
+import io.crowdcode.flaschenlager.stock.model.Stock;
+import io.crowdcode.flaschenlager.stock.model.StockEntryQuantity;
+import io.crowdcode.flaschenlager.stock.model.StockEntryResponse;
+import io.crowdcode.flaschenlager.stock.service.StockService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+
+    @Autowired
+    private StockService stockService;
+
+    @ApiOperation("Get all stocks")
+    @GetMapping
+    public List<Stock> getStocks() {
+        return stockService.findAllStocks();
+    }
+
+    @ApiOperation("Create new stock")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createStock(@RequestBody @Valid Stock stock) {
+        stockService.registerStock(stock.getName());
+    }
+
+    @ApiOperation("Get stock")
+    @GetMapping(path = "/{stockId}")
+    public Stock getStock(@PathVariable("stockId") Long stockId) {
+        return stockService.findStock(stockId);
+    }
+
+    @ApiOperation("Get available product quantities")
+    @GetMapping(path = "/{stockId}/entries")
+    public List<StockEntryQuantity> getAvailableProductQuantities(@PathVariable("stockId") Long stockId) {
+        return stockService.quantityOfAvailableProducts(stockId);
+    }
+
+    @ApiOperation("Receipt products")
+    @PutMapping(path = "/{stockId}/entries")
+    public void putToStock(@PathVariable("stockId") Long stockId, @RequestBody @Valid EntryReceiptRequest entry) {
+        stockService.put(stockId, entry.getProductId(), entry.getQuantity(), entry.getPrice().doubleValue());
+    }
+
+    @ApiOperation("Pull product from stock.")
+    @PostMapping(path = "/{stockId}/entries")
+    public List<StockEntryResponse> getPull(@PathVariable("stockId") Long stockId,
+                                            @RequestBody @Valid EntryPullRequest entryPullRequest) {
+        return stockService.pull(stockId, entryPullRequest.getProductId(), entryPullRequest.getQuantity());
+    }
+
+
+}
