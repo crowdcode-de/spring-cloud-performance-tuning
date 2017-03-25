b1b3091ca02bb909c45befa68a4a1930bc6ff308
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-25 20:27:04 +0100

    add equals, hashcode, and toString to EntryQuantity

--- stock-service/src/main/java/io/crowdcode/flaschenlager/stock/model/StockEntryQuantity.java
+++ stock-service/src/main/java/io/crowdcode/flaschenlager/stock/model/StockEntryQuantity.java
@@ -1,13 +1,17 @@
 package io.crowdcode.flaschenlager.stock.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 @Accessors(chain = true)
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,5 +20,6 @@ public class StockEntryQuantity {
     private Long productId;
 
     private Long quantity;
+
 
 }
