680b46c66abfc23360a60eb610ea26da7d219ffe
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-26 01:21:42 +0100

    add jpa repository, and some tests

---
+++ accounting-service/src/main/java/io/crowdcode/flaschenlager/accounting/repository/AccountEntryRepository.java
@@ -0,0 +1,16 @@
+package io.crowdcode.flaschenlager.accounting.repository;
+
+import io.crowdcode.flaschenlager.accounting.model.AccountEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AccountEntryRepository extends JpaRepository<AccountEntry, Long> {
+
+    List<AccountEntry> findByCustomerIdAndUserId(@Param("customerId") Long customerId,
+                                                 @Param("userId") Long userId);
+
+    List<AccountEntry> findByCustomerId(@Param("customerId") Long customerId);
+
+}
---
+++ accounting-service/src/test/java/io/crowdcode/flaschenlager/accounting/repository/AccountEntryRepositoryTest.java
@@ -0,0 +1,60 @@
+package io.crowdcode.flaschenlager.accounting.repository;
+
+import io.crowdcode.flaschenlager.accounting.model.AccountEntry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static io.crowdcode.flaschenlager.accounting.fixture.AccountEntryFixture.buildAccountEntry;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AccountEntryRepositoryTest {
+
+    @Autowired
+    private AccountEntryRepository accountEntryRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void testSaveAccountEntry() throws Exception {
+        AccountEntry entry = accountEntryRepository.save(
+                buildAccountEntry(1l, 2l));
+
+        assertThat(entry.getId(), is(notNullValue()));
+    }
+
+    @Test
+    public void testFindByCustomerIdAndUserId() throws Exception {
+        testEntityManager.persist(buildAccountEntry(1l, 1l));
+        testEntityManager.persist(buildAccountEntry(1l, 2l));
+        testEntityManager.persist(buildAccountEntry(1l, 3l));
+        testEntityManager.persist(buildAccountEntry(2l, 4l));
+
+        List<AccountEntry> entries = accountEntryRepository.findByCustomerIdAndUserId(1l, 3l);
+
+        assertThat(entries, hasSize(1));
+    }
+
+    @Test
+    public void testFindByCustomerId() throws Exception {
+        testEntityManager.persist(buildAccountEntry(1l, 1l));
+        testEntityManager.persist(buildAccountEntry(1l, 2l));
+        testEntityManager.persist(buildAccountEntry(1l, 3l));
+        testEntityManager.persist(buildAccountEntry(2l, 4l));
+
+        List<AccountEntry> entries = accountEntryRepository.findByCustomerId(1l);
+
+        assertThat(entries, hasSize(3));
+    }
+
+
+}
