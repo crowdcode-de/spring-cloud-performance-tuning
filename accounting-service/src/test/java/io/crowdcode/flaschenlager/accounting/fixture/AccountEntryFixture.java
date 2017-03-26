6b4e1280867e6d32916a50d5c86101517e5b1d49
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-26 01:21:15 +0100

    add fixture for account entries.

---
+++ accounting-service/src/test/java/io/crowdcode/flaschenlager/accounting/fixture/AccountEntryFixture.java
@@ -0,0 +1,28 @@
+package io.crowdcode.flaschenlager.accounting.fixture;
+
+import io.crowdcode.flaschenlager.accounting.model.AccountEntry;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class AccountEntryFixture {
+
+    private static long now = System.currentTimeMillis();
+    private static int nanoOfSecond = 0;
+
+
+    public static AccountEntry buildAccountEntry(Long customerId, Long userId) {
+        return new AccountEntry()
+                .setCustomerId(customerId)
+                .setUserId(userId)
+                .setQuantity(10l)
+                .setPrice(BigDecimal.valueOf(1.2d))
+                .setDescription("Description")
+                .setBookedAt(now());
+    }
+
+    private static LocalDateTime now() {
+        return LocalDateTime.ofEpochSecond(now, nanoOfSecond++, ZoneOffset.UTC);
+    }
+}
