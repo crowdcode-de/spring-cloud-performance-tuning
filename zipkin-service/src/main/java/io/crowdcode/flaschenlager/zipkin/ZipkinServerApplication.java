f34131c9a291aad3964c6fc34c75f82b42c0f709
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-28 18:46:19 +0200

    add pom.xml

---
+++ zipkin-service/src/main/java/io/crowdcode/flaschenlager/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,10 @@
+package io.crowdcode.flaschenlager.zipkin;
+
+//@EnableZipkinServer
+//@EnableDiscoveryClient
+//@SpringBootApplication
+public class ZipkinServerApplication {
+    public static void main(String[] args) {
+//        SpringApplication.run(ZipkinServiceApplication.class, args);
+    }
+}
