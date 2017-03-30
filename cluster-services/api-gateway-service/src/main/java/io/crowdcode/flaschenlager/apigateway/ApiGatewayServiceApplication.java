b57ecca8af872710adef207167242f32809f3c0f
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-30 23:28:59 +0200

    update zuul configuration

--- cluster-services/api-gateway-service/src/main/java/io/crowdcode/flaschenlager/apigateway/ApiGatewayServiceApplication.java
+++ cluster-services/api-gateway-service/src/main/java/io/crowdcode/flaschenlager/apigateway/ApiGatewayServiceApplication.java
@@ -3,8 +3,12 @@ package io.crowdcode.flaschenlager.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 
+@EnableZuulServer
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
