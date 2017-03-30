2cf469b9b47b28ea80350cf08ab30d10ca69fbdc
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-03-30 22:33:03 +0200

    rename ApiGatewwayConfiguration into OrderConfiguration

--- domain-services/order-service/src/main/java/io/crowdcode/flaschenlager/order/config/OrderConfiguration.java
+++ domain-services/order-service/src/main/java/io/crowdcode/flaschenlager/order/config/OrderConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ApiGatewayConfiguration {
+public class OrderConfiguration {
 
     @Bean
     @LoadBalanced
